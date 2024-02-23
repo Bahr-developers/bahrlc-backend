2265423f231c3bb772f8429b4c59ebf1fbc48a64
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2024-02-23 19:43:41 +0500

    language module add

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+import { LanguageModule } from '@modules';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://0.0.0.0:27017/bahrtech'),
+    LanguageModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app';
 import { appConfig } from '@config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Bahr LC')
     .setDescription('The Bahr Learning Centre API description')
     .setVersion('1.0')
     .addTag('bahrlc')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
