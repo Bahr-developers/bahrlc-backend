da2d838505e2dfcde73c837dd0f985b599e4102f
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2024-04-18 14:58:02 +0500

    fixed file upload on swagger

--- src/modules/projects/project.controller.ts
+++ src/modules/projects/project.controller.ts
@@ -1,61 +1,67 @@
 import {
-    Body,
-    Controller,
-    Post,
-    Get,
-    Patch,
-    Param,
-    Delete,
-    Headers,
-    UseInterceptors,
-    UploadedFiles
-  } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Delete,
+  Headers,
+  UseInterceptors,
+  UploadedFiles,
+} from '@nestjs/common';
+import { ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { CreateProjectDto, UpdateProjectDto } from './dtos';
 import { Project } from './schemas';
 import { ProjectService } from './project.service';
 import { FilesInterceptor } from '@nestjs/platform-express';
-  
-  @ApiTags('Project')
-  @Controller({
-    path: 'project',
-    version: '1.0',
-  })
-  export class ProjectController {
-    #_projectService: ProjectService;
-  
-    constructor(project: ProjectService) {
-      this.#_projectService = project;
-    }
-  
-    @Post('add')
-    @UseInterceptors(FilesInterceptor('images'))
-    createProject(
-      @Body() payload: CreateProjectDto,
-      @UploadedFiles() images: any[]
-    ): Promise<void> {
-      return this.#_projectService.createProject({...payload, images});
-    }
 
-    @Get('find/all')
-    async getServiceList(
-      @Headers('accept-language') languageCode: string
-      ): Promise<Project[]> {
-        return await this.#_projectService.getProjectList(languageCode);
-    }
+@ApiTags('Project')
+@Controller({
+  path: 'project',
+  version: '1.0',
+})
+export class ProjectController {
+  #_projectService: ProjectService;
 
-    @Patch('edit/:id')
-    @UseInterceptors(FilesInterceptor('images'))
-    async updateProject(
-      @Param('id') projectId: string,
-      @Body() payload: UpdateProjectDto,
-      @UploadedFiles() images: any[]
-    ): Promise<void> {
-      await this.#_projectService.updateProject({ ...payload, id: projectId, images });
-    }
-  
-    @Delete('delete/:id')
-    async deleteProject(@Param('id') projectId: string): Promise<void> {
-      await this.#_projectService.deleteProject(projectId);
-    }
+  constructor(project: ProjectService) {
+    this.#_projectService = project;
+  }
+
+  @ApiConsumes('multipart/form-data')
+  @Post('add')
+  @UseInterceptors(FilesInterceptor('images'))
+  createProject(
+    @Body() payload: CreateProjectDto,
+    @UploadedFiles() images: any[],
+  ): Promise<void> {
+    return this.#_projectService.createProject({ ...payload, images });
+  }
+
+  @Get('find/all')
+  async getServiceList(
+    @Headers('accept-language') languageCode: string,
+  ): Promise<Project[]> {
+    return await this.#_projectService.getProjectList(languageCode);
+  }
+
+  @ApiConsumes('multipart/form-data')
+  @Patch('edit/:id')
+  @UseInterceptors(FilesInterceptor('images'))
+  async updateProject(
+    @Param('id') projectId: string,
+    @Body() payload: UpdateProjectDto,
+    @UploadedFiles() images: any[],
+  ): Promise<void> {
+    await this.#_projectService.updateProject({
+      ...payload,
+      id: projectId,
+      images,
+    });
+  }
+
+  @Delete('delete/:id')
+  async deleteProject(@Param('id') projectId: string): Promise<void> {
+    await this.#_projectService.deleteProject(projectId);
+  }
 }
